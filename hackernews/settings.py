26b2ed469830806bb2249f400a7ee9ec029531bd
RAFAELMH809 <melchorhernandezrafael057@gmail.com>
2025-05-26 18:10:43 -0600

    UBA

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -165,11 +165,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+   
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATIC_URL = '/static/'
+    
+    #STATIC_URL = '/static/'
     #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 GRAPHENE = {
     'SCHEMA': 'mysite.myschema.schema',
